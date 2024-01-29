a6fdc397f017827bc58102d8aeecb73473c0c247
SeongHwi Cho <hwi7141@gmail.com>
2024-01-29 16:20:12 +0900

    add game app

---
+++ reminder/pikaPong/game/routing.py
@@ -0,0 +1,8 @@
+
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path('pong/', consumers.PongConsumer.as_asgi()),
+]
--- reminder/pikaPong/pikaPong/asgi.py
+++ reminder/pikaPong/pikaPong/asgi.py
@@ -1,16 +1,35 @@
 """
-ASGI config for pikaPong pikaPong.
+ASGI config for multi_pong project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pikaPong.settings')
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multi_pong.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+import game.routing
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(game.routing.websocket_urlpatterns)
+        )
+    ),
+})
